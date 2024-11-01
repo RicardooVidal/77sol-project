b8667f2a599cd8796c5e99696640e223ba23ae14
RicardooVidal <ricardoorv95@gmail.com>
2024-10-31 22:15:13 -0300

    chore: adding migrations and fix docker-compose

---
+++ database/migrations/2024_11_01_001456_create_projects_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('projects', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('customer_id');
+            $table->unsignedBigInteger('type_instalation_id');
+            $table->string('location', 2);
+            $table->timestamps();
+
+            $table->foreign('customer_id')->references('id')->on('customers');
+            $table->foreign('type_instalation_id')->references('id')->on('types_instalations');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('projects');
+    }
+};
